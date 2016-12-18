3a558b03b2c828dc403ca792100a4e7dd7a503be
Max <ikj1234i@yahoo.com>
2016-12-18 10:16:22 -0500

    bugfix

--- op25/gr-op25_repeater/lib/software_imbe_decoder.cc
+++ op25/gr-op25_repeater/lib/software_imbe_decoder.cc
@@ -962,7 +962,7 @@ software_imbe_decoder::decode_tap(int _L, int _K, float _w0, const int * _v, con
          if(abs((int)sample) > 32767) {
             sample = (sample < 0) ? -32767 : 32767; // * sgn(sample)
          }
-         samples->push_back((short)sample);
+         samples->push_back(sample);
       }
    OldL = L;
    Oldw0 = w0;
