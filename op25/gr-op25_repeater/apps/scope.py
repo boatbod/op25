87f546a9477653dc974eda2e98e6c38f141252a0
Max <ikj1234i@yahoo.com>
2017-10-18 19:11:53 -0400

    scope.py message update

--- op25/gr-op25_repeater/apps/scope.py
+++ op25/gr-op25_repeater/apps/scope.py
@@ -3,10 +3,13 @@
 s="""
 *****************************************************************************
 *
-* The scope.py app has been deprecated; it has been replaced by rx.py.
+* The scope.py app has been deprecated; it has been replaced by rx.py .
+*
+* See file "README" in this directory for further information about rx.py .
+*
 * If you really want to use scope.py, 
 * 1) 'sudo make uninstall' the currently installed version of OP25
-* 2) 'get checkout bcef5dfc295a76aa219709c3492285b81652a2dd'
+* 2) 'git checkout bcef5dfc295a76aa219709c3492285b81652a2dd'
 * 3) rebuild and re-install OP25, starting with an empty 'build' directory
 *
 *****************************************************************************
