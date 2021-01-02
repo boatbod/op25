boatbod/op25

commit c869eb057689d664fce676d7747ea1eebb677e8b

Compute and track AMBE error rates, frame repeats & muting per TIA-102-BABA-A 14.6-14.9

op25/gr-op25_repeater/lib/rx_sync.cc
@@ -214,8 +214,12 @@ rx_sync::rx_sync(const char * options, int debug, int msgq_id, gr::msg_queue::sp
 
 	mbe_initMbeParms (&cur_mp[0], &prev_mp[0], &enh_mp[0]);
 	mbe_initMbeParms (&cur_mp[1], &prev_mp[1], &enh_mp[1]);
+	mbe_initErrParms (&errs_mp[0]);
+	mbe_initErrParms (&errs_mp[1]);
 	mbe_initToneParms (&tone_mp[0]);
 	mbe_initToneParms (&tone_mp[1]);
+	mbe_err_cnt[0] = 0;
+	mbe_err_cnt[1] = 0;
 	sync_reset();
 }
 
@@ -280,7 +284,7 @@ void rx_sync::codeword(const uint8_t* cw, const enum codeword_types codeword_typ
 	uint8_t tmp_codeword [144];
 	uint32_t E0, ET;
 	uint32_t u[8];
-	size_t errs = 0;
+	size_t errs = 0, rc = 0;
 	bool do_fullrate = false;
 	bool do_silence = false;
 	bool do_tone = false;
@@ -289,13 +293,13 @@ void rx_sync::codeword(const uint8_t* cw, const enum codeword_types codeword_typ
 
 	switch(codeword_type) {
 	case CODEWORD_DMR:
-		errs = interleaver.process_vcw(cw, b, U);
+		errs = interleaver.process_vcw(&errs_mp[slot_id], cw, b, U);
 		interleaver.pack_cw(p_cw, U);
 		if (d_debug >= 9) {
-			fprintf(stderr, "%s AMBE %02x %02x %02x %02x %02x %02x %02x errs %lu\n", logts.get(d_msgq_id),
-			       	p_cw[0], p_cw[1], p_cw[2], p_cw[3], p_cw[4], p_cw[5], p_cw[6], errs);
+			fprintf(stderr, "%s AMBE %02x %02x %02x %02x %02x %02x %02x errs %lu err_rate %f\n", logts.get(d_msgq_id),
+			       	p_cw[0], p_cw[1], p_cw[2], p_cw[3], p_cw[4], p_cw[5], p_cw[6], errs, errs_mp[slot_id].ER);
 		}
-		if (d_slot_key) {
+		if (d_slot_key) { // BP reversal if key is specified
 			uint8_t skipped_bits = p_cw[1] & 0xf0;
 			for (int i = 0; i <= 6; i++)
 				p_cw[i]   ^= (d_slot_key >> ((i + 1) % 2) * 8);
@@ -305,21 +309,50 @@ void rx_sync::codeword(const uint8_t* cw, const enum codeword_types codeword_typ
 			interleaver.pack_cw(p_cw, U);
 
 			if (d_debug >= 9) {
-				fprintf(stderr, "%s ambe %02x %02x %02x %02x %02x %02x %02x errs %lu\n", logts.get(d_msgq_id),
-			       		p_cw[0], p_cw[1], p_cw[2], p_cw[3], p_cw[4], p_cw[5], p_cw[6], errs);
+				fprintf(stderr, "%s ambe^%02x^%02x^%02x^%02x^%02x^%02x^%02x\n", logts.get(d_msgq_id),
+			       		p_cw[0], p_cw[1], p_cw[2], p_cw[3], p_cw[4], p_cw[5], p_cw[6]);
 			}
 		}
 
-		if (mbe_dequantizeAmbeTone(&tone_mp[slot_id], U) == 0) {
-			do_tone = true;
-		} else if (b[0] < 120) { // TODO: handle Erasures/Frame Repeat
-			mbe_dequantizeAmbe2250Parms(&cur_mp[slot_id], &prev_mp[slot_id], b);
+		// handle frame repeats, tones and voice
+		rc = mbe_dequantizeAmbeTone(&tone_mp[slot_id], &errs_mp[slot_id], U);
+		if (rc >= 0) {					// Tone Frame
+			if (rc == 0) {                  // Valid Tone
+				do_tone = true;
+				mbe_err_cnt[slot_id] = 0;
+			} else {                        // Tone Erasure with Frame Repeat
+				if ((++mbe_err_cnt[slot_id] < 4) && do_tone) {
+					mbe_useLastMbeParms(&cur_mp[slot_id], &prev_mp[slot_id]);
+					rc = 0;
+				} else {
+					do_tone = false;        // Mute audio output after 3 successive Frame Repeats
+					do_silence = true;
+				}
+ 			}
+		} else {
+			rc = mbe_dequantizeAmbe2250Parms (&cur_mp[slot_id], &prev_mp[slot_id], &errs_mp[slot_id], b);
+			if (rc == 0) {				// Voice Frame
+				do_tone = false;
+				mbe_err_cnt[slot_id] = 0;
+			} else if ((++mbe_err_cnt[slot_id] < 4) && !do_tone) {// Erasure with Frame Repeat per TIA-102.BABA.5.6
+				mbe_useLastMbeParms(&cur_mp[slot_id], &prev_mp[slot_id]);
+				rc = 0;
+			} else {
+				do_tone = false;            // Mute audio output after 3 successive Frame Repeats
+				do_silence = true;
+			}
+		}
+		if (errs_mp[slot_id].ER > 0.096) { // Mute if error rate exceeds threshold
+			do_tone = false;
+			do_silence = true;
 		}
 		break;
 	case CODEWORD_DSTAR:
 		interleaver.decode_dstar(cw, b, false);
-		if (b[0] < 120)
-			mbe_dequantizeAmbe2400Parms(&cur_mp[slot_id], &prev_mp[slot_id], b);
+		if (b[0] < 120) // TODO: frame repeats and tones
+			mbe_dequantizeAmbe2400Parms(&cur_mp[slot_id], &prev_mp[slot_id], &errs_mp[slot_id], b);
+		else
+			do_silence = true;
 		break;
 	case CODEWORD_YSF_HALFRATE:	// 104 bits
 		for (int i=0; i<x; i++) {
@@ -332,8 +365,10 @@ void rx_sync::codeword(const uint8_t* cw, const enum codeword_types codeword_typ
 
 		memcpy(tmp_codeword+27, buf+81, 22);
 		decode_49bit(b, tmp_codeword);
-		if (b[0] < 120)
-			mbe_dequantizeAmbe2250Parms(&cur_mp[slot_id], &prev_mp[slot_id], b);
+		if (b[0] < 120) // TODO: frame repeats and tones
+			mbe_dequantizeAmbe2250Parms(&cur_mp[slot_id], &prev_mp[slot_id], &errs_mp[slot_id], b);
+		else
+			do_silence = true;
 		break;
 	case CODEWORD_P25P2:
 		break; // Not used; handled by p25p2_tdma
@@ -353,9 +388,7 @@ void rx_sync::codeword(const uint8_t* cw, const enum codeword_types codeword_typ
 		if (do_fullrate) {
 			d_software_decoder[slot_id].decode(fullrate_cw);
 		} else {	/* halfrate */
-			if (b[0] >= 120) {
-				do_silence = true;
-			} else {
+			if (!do_silence) {
 				d_software_decoder[slot_id].decode_tap(cur_mp[slot_id].L, 0, cur_mp[slot_id].w0, &cur_mp[slot_id].Vl[1], &cur_mp[slot_id].Ml[1]);
 			}
 		}

op25/gr-op25_repeater/lib/ambe.c
@@ -21,7 +21,7 @@
 #include "ambe3600x2400_const.h"
 
 static int
-mbe_dequantizeAmbeParms (mbe_parms * cur_mp, mbe_parms * prev_mp, const int *b, int dstar)
+mbe_dequantizeAmbeParms (mbe_parms* cur_mp, mbe_parms* prev_mp, mbe_errs* errs, const int *b, int dstar)
 {
 
   int ji, i, j, k, l, L, L9, m, am, ak;
@@ -54,12 +54,14 @@ mbe_dequantizeAmbeParms (mbe_parms * cur_mp, mbe_parms * prev_mp, const int *b, 
   // copy repeat from prev_mp
   cur_mp->repeat = prev_mp->repeat;
 
-  if ((b0 >= 120) && (b0 <= 123))
+  if (((b0 >= 120) && (b0 <= 123)) || \
+       (errs->E0 >= 4) || \
+      ((errs->E0 >= 2) && ((errs->E0 + errs->E1) >= 6)))
     {
 #ifdef AMBE_DEBUG
       fprintf (stderr, "AMBE Erasure Frame\n");
 #endif
-      return (2);
+      return 1;
     }
   else if ((b0 == 124) || (b0 == 125))
     {
@@ -79,9 +81,10 @@ mbe_dequantizeAmbeParms (mbe_parms * cur_mp, mbe_parms * prev_mp, const int *b, 
   else if ((b0 == 126) || (b0 == 127))
     {
 #ifdef AMBE_DEBUG
+      // should never get here because tones need to be identified before voice
       fprintf (stderr, "AMBE Tone Frame\n");
 #endif
-      return (3);
+      return 3;
     }
 
   if (silence == 0)
@@ -414,15 +417,16 @@ mbe_dequantizeAmbeParms (mbe_parms * cur_mp, mbe_parms * prev_mp, const int *b, 
 }
 
 int
-mbe_dequantizeAmbeTone(mbe_tone * tone, const int *u)
+mbe_dequantizeAmbeTone(mbe_tone* tone, mbe_errs* errs, const int *u)
 {
 	int bitchk1, bitchk2;
 	int AD, ID0, ID1, ID2, ID3, ID4;
 	bitchk1 = (u[0] >> 6) & 0x3f;
 	bitchk2 = (u[3] & 0xf);
 
+	// Check if tone frame
 	if ((bitchk1 != 63) || (bitchk2 != 0))
-		return -1; // Not a valid tone frame
+		return -1; // Not a tone
 
 	AD = ((u[0] & 0x3f) << 1) + ((u[3] >> 4) & 0x1);
 	ID0 = 0;
@@ -430,6 +434,11 @@ mbe_dequantizeAmbeTone(mbe_tone * tone, const int *u)
 	ID2 = ((u[1] & 0xf) << 4) + ((u[2] >> 7) & 0xf);
 	ID3 = ((u[2] & 0x7f) << 1) + ((u[3] >> 13) & 0x1);
 	ID4 = ((u[3] & 0x1fe0) >> 5);
+
+	// Check error thresholds
+	if ((errs->E0 >= 4) || ((errs->E0 >= 2) && ((errs->E0 + errs->E1) >= 6))) {
+		return 1; // Uncorrectable error threshold, treat as Erasure
+	}
 
 	// Theorectically ID1-4 should all be the same value.  Make sure at least 3 match
 	if (((ID1 == ID2) && (ID1 == ID3)) || \
@@ -458,13 +467,13 @@ mbe_dequantizeAmbeTone(mbe_tone * tone, const int *u)
 }
 
 int
-mbe_dequantizeAmbe2400Parms (mbe_parms * cur_mp, mbe_parms * prev_mp, const int *b){
+mbe_dequantizeAmbe2400Parms (mbe_parms* cur_mp, mbe_parms* prev_mp, mbe_errs* errs, const int *b){
 	int dstar = 1;
-	return (mbe_dequantizeAmbeParms (cur_mp, prev_mp, b, dstar));
+	return (mbe_dequantizeAmbeParms (cur_mp, prev_mp, errs, b, dstar));
 }
 
 int
-mbe_dequantizeAmbe2250Parms (mbe_parms * cur_mp, mbe_parms * prev_mp, const int *b){
+mbe_dequantizeAmbe2250Parms (mbe_parms* cur_mp, mbe_parms* prev_mp, mbe_errs* errs, const int *b){
 	int dstar = 0;
-	return (mbe_dequantizeAmbeParms (cur_mp, prev_mp, b, dstar));
+	return (mbe_dequantizeAmbeParms (cur_mp, prev_mp, errs, b, dstar));
 }

op25/gr-op25_repeater/lib/ambe.h
@@ -3,9 +3,9 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-int mbe_dequantizeAmbe2250Parms (mbe_parms * cur_mp, mbe_parms * prev_mp, const int *b);
-int mbe_dequantizeAmbe2400Parms (mbe_parms * cur_mp, mbe_parms * prev_mp, const int *b);
-int mbe_dequantizeAmbeTone(mbe_tone * tone, const int *u);
+int mbe_dequantizeAmbe2250Parms (mbe_parms* cur_mp, mbe_parms* prev_mp, mbe_errs* errs, const int *b);
+int mbe_dequantizeAmbe2400Parms (mbe_parms* cur_mp, mbe_parms* prev_mp, mbe_errs* errs, const int *b);
+int mbe_dequantizeAmbeTone(mbe_tone* tone, mbe_errs* errs, const int *u);
 #ifdef __cplusplus
 }
 #endif

op25/gr-op25_repeater/lib/ambe_encoder.cc
@@ -155,7 +155,7 @@ static inline float make_f0(int b0) {
 	return (powf(2, (-4.311767578125 - (2.1336e-2 * ((float)b0+0.5)))));
 }
 
-static void encode_ambe(const IMBE_PARAM *imbe_param, int b[], mbe_parms*cur_mp, mbe_parms*prev_mp, bool dstar, float gain_adjust) {
+static void encode_ambe(const IMBE_PARAM *imbe_param, int b[], mbe_parms*cur_mp, mbe_parms*prev_mp, mbe_errs* errs_mp, bool dstar, float gain_adjust) {
 	static const float SQRT_2 = sqrtf(2.0);
 	static const int b0_lmax = sizeof(b0_lookup) / sizeof(b0_lookup[0]);
 	// int b[9];
@@ -484,9 +484,9 @@ static void encode_ambe(const IMBE_PARAM *imbe_param, int b[], mbe_parms*cur_mp,
 	// fprintf (stderr, "B\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n", b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8]);
 	int rc;
 	if (dstar)
-		rc = mbe_dequantizeAmbe2400Parms (cur_mp, prev_mp, b);
+		rc = mbe_dequantizeAmbe2400Parms (cur_mp, prev_mp, errs_mp, b);
 	else
-		rc = mbe_dequantizeAmbe2250Parms (cur_mp, prev_mp, b);
+		rc = mbe_dequantizeAmbe2250Parms (cur_mp, prev_mp, errs_mp, b);
 	mbe_moveMbeParms (cur_mp, prev_mp);
 }
 
@@ -550,6 +550,7 @@ ambe_encoder::ambe_encoder(void)
 {
 	mbe_parms enh_mp;
 	mbe_initMbeParms (&cur_mp, &prev_mp, &enh_mp);
+	mbe_initErrParms (&errs_mp);
 }
 
 void ambe_encoder::set_dstar_mode(void)
@@ -577,7 +578,7 @@ void ambe_encoder::encode(int16_t samples[], uint8_t codeword[])
 	vocoder.imbe_encode(frame_vector, samples);
 
 	// halfrate audio encoding - output rate is 2450 (49 bits)
-	encode_ambe(vocoder.param(), b, &cur_mp, &prev_mp, d_dstar_mode, d_gain_adjust);
+	encode_ambe(vocoder.param(), b, &cur_mp, &prev_mp, &errs_mp, d_dstar_mode, d_gain_adjust);
 
 	if (d_dstar_mode) {
 		interleaver.encode_dstar(codeword, b, d_alt_dstar_interleave);

op25/gr-op25_repeater/lib/ambe_encoder.h
@@ -35,6 +35,7 @@ private:
 	p25p2_vf interleaver;
 	mbe_parms cur_mp;
 	mbe_parms prev_mp;
+	mbe_errs errs_mp;
 	bool d_49bit_mode;
 	bool d_dstar_mode;
 	float d_gain_adjust;

op25/gr-op25_repeater/lib/d2460.cc
@@ -40,6 +40,7 @@ static p25p2_vf interleaver;
 static mbe_parms cur_mp;
 static mbe_parms prev_mp;
 static mbe_parms enh_mp;
+static mbe_errs errs_mp;
 
 static const Uns DV3K_START_BYTE = 0x61;
 enum
@@ -144,6 +145,7 @@ static void vocoder_setup(void) {
 	encoder.set_gain_adjust(GAIN_ADJUST);
 	encoder.set_alt_dstar_interleave(true);
 	mbe_initMbeParms (&cur_mp, &prev_mp, &enh_mp);
+	mbe_initErrParms (&errs_mp);
 }
 
 static void dump(unsigned char *p, ssize_t n)
@@ -254,7 +256,7 @@ static Uns pkt_process(Uns*pkt, Uns cnt)
             memset(6+(char*)pkt, 0, 320);   // silence
             // FIXME: add handling for tone case (b0=126)
         } else {
-            rc = mbe_dequantizeAmbe2400Parms(&cur_mp, &prev_mp, b);
+            rc = mbe_dequantizeAmbe2400Parms(&cur_mp, &prev_mp, &errs_mp, b);
             printf("B\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n", b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8]);
             K = 12;
             if (cur_mp.L <= 36)

op25/gr-op25_repeater/lib/gardner_costas_cc_impl.cc
@@ -244,9 +244,8 @@ gardner_costas_cc_impl::phase_error_tracking(gr_complex sample)
   d_freq = gr::branchless_clip(d_freq, d_max_freq);
   
 #if VERBOSE_COSTAS
-  fprintf(stderr, "cl: phase_error: %f  phase: %f  freq: %f  sample: %f+j%f  constellation: %f+j%f\n",
-	 phase_error, d_phase, d_freq, sample.real(), sample.imag(), 
-	 d_constellation[d_current_const_point].real(), d_constellation[d_current_const_point].imag());
+  fprintf(stderr, "cl: phase_error: %f  phase: %f  freq: %f  sample: %f+j%f\n",
+	 phase_error, d_phase, d_freq, sample.real(), sample.imag());
 #endif
 }
 

op25/gr-op25_repeater/lib/mbelib.c
@@ -101,6 +101,14 @@ mbe_initToneParms (mbe_tone * tone_mp)
 }
 
 void
+mbe_initErrParms (mbe_errs * errs_mp)
+{
+  errs_mp->E0 = 0;
+  errs_mp->E1 = 0;
+  errs_mp->ER = 0;
+}
+
+void
 mbe_spectralAmpEnhance (mbe_parms * cur_mp)
 {
 

commit 36b10db90c9578f0c27a4e33abd5f447b4a7edc7

Bugfix for DMR AMBE decode regression caused by c869eb057689d664fce676d7747ea1eebb677e8b

op25/gr-op25_repeater/lib/rx_sync.cc
@@ -296,7 +296,8 @@ void rx_sync::codeword(const uint8_t* cw, const enum codeword_types codeword_typ
 	uint8_t tmp_codeword [144];
 	uint32_t E0, ET;
 	uint32_t u[8];
-	size_t errs = 0, rc = 0;
+	size_t errs = 0;
+    int rc = 0;
 	bool do_fullrate = false;
 	bool do_silence = false;
 	bool do_tone = false;