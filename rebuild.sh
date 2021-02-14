b61b235fa6545b785478e08c45f0e67c893fb3ae
gnorbury <gnorbury@bondcar.com>
2021-02-14 15:52:57 -0500

    Allow selection of debug or release build type

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
+
+usage()
+{
+  echo "Usage: $0 [-d|-r]"
+  echo "  -d   Debug build"
+  echo "  -r   Release build (default)"
+  exit 2
+}
+
+BUILD_TYPE=""
+while getopts 'dr?h' c
+do
+  case $c in
+    d) BUILD_TYPE="-DCMAKE_BUILD_TYPE=Debug" ;;
+    r) BUILD_TYPE="-DCMAKE_BUILD_TYPE=Release" ;;
+    h|?) usage ;;
+  esac
+done
+
 git pull
 cd build
 rm -rf *
-cmake ../         2>&1 | tee cmake.log
-make              2>&1 | tee make.log
-sudo make install 2>&1 | tee install.log
+cmake ../ $BUILD_TYPE 2>&1 | tee cmake.log
+make                  2>&1 | tee make.log
+sudo make install     2>&1 | tee install.log
 sudo ldconfig
