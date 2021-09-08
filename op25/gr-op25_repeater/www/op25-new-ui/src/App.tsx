a11c0bbcca9ee63a10032dc9158983dab4872067
Gabriel Graves <nebraskacoder@gmail.com>
2021-09-08 01:51:19 -0500

    Send commands testing

--- op25/gr-op25_repeater/www/op25-new-ui/src/App.tsx
+++ op25/gr-op25_repeater/www/op25-new-ui/src/App.tsx
@@ -3,13 +3,10 @@ import TopMenuBarAndDrawers from "./components/TopMenuBarAndDrawers";
 import { useAppDispatch, useAppSelector } from "redux/app/hooks";
 import { isMenuDrawerOpen } from "redux/slices/interface/interfaceSlice";
 import ReceiverUi from "pages/ReceiverUi";
-import {
-  addToSendQueue,
-  selectChannels,
-  sendQueue,
-} from "redux/slices/op25/op25Slice";
+import { addToSendQueue, sendQueue } from "redux/slices/op25/op25Slice";
 
 import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import { OP25 } from "lib/op25";
 
 interface useStylesProps {
   isOpen: boolean;
@@ -42,37 +39,43 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const App = () => {
+  const op25 = OP25.getInstance();
   const dispatch = useAppDispatch();
-  const channels = useAppSelector(selectChannels);
   const isOpen = useAppSelector(isMenuDrawerOpen);
   const classes = useStyles({ isOpen });
 
   useEffect(() => {
-    dispatch(addToSendQueue({ command: "get_config", arg1: 0, arg2: 0 }));
-
     const updateTimer = setInterval(async () => {
-      if (channels.length === 0) {
-        await dispatch(addToSendQueue({ command: "update", arg1: 0, arg2: 0 }));
-      } else {
-        channels.forEach(async (channel) => {
-          await dispatch(
-            addToSendQueue({
-              command: "update",
-              arg1: 0,
-              arg2: channel.id,
-            })
-          );
-        });
-      }
+      op25.sendUpdateChannels();
     }, 1000);
 
     const sendQueueTimer = setInterval(async () => {
       await dispatch(sendQueue());
     }, 1000);
 
+    const testVerbosity = setTimeout(async () => {
+      await op25.sendSetDebugOnChannel(0, 10);
+    }, 500);
+    const testHoldChannel = setTimeout(async () => {
+      //await op25.sendHoldOnChannel(0, 5);
+      await op25.sendHoldOnChannel(1, 7);
+    }, 3000);
+
+    const testSkipChannel = setTimeout(async () => {
+      //await op25.sendSkipOnChannel(0);
+      //await op25.sendSkipOnChannel(1);
+      //await op25.sendHoldOnChannel(0, 0);
+      //await op25.sendHoldOnChannel(1, 0);
+      //await op25.sendUnHoldOnChannel(0);
+      await op25.sendUnHoldOnChannel(1);
+    }, 5000);
+
     return () => {
+      clearTimeout(testVerbosity);
       clearInterval(updateTimer);
       clearInterval(sendQueueTimer);
+      clearTimeout(testHoldChannel);
+      clearTimeout(testSkipChannel);
     };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
