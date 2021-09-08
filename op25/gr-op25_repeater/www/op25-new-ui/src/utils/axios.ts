dfb4fe3ccad2daf767e3ea93f6e331e7a9b1b910
Gabriel Graves <nebraskacoder@gmail.com>
2021-09-07 23:04:17 -0500

    Added ability to change api url from .env file

--- op25/gr-op25_repeater/www/op25-new-ui/src/utils/axios.ts
+++ op25/gr-op25_repeater/www/op25-new-ui/src/utils/axios.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const Axios = () => {
   return axios.create({
-    baseURL: "/",
+    baseURL: process.env.REACT_APP_DEV_SERVER_API
+      ? process.env.REACT_APP_DEV_SERVER_API
+      : "/",
     method: "post",
     headers: { "Content-type": "application/json" },
     timeout: 5000,
