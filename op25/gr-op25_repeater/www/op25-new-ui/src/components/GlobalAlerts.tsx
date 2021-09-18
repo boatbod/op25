f5440027dfca29ea2d1ced4c84d468dc88169066
Gabriel Graves <nebraskacoder@gmail.com>
2021-09-18 04:14:11 -0500

    Modify alerts display

--- op25/gr-op25_repeater/www/op25-new-ui/src/components/GlobalAlerts.tsx
+++ op25/gr-op25_repeater/www/op25-new-ui/src/components/GlobalAlerts.tsx
@@ -1,32 +1,80 @@
+import { SyntheticEvent, useEffect, useState } from "react";
 import { Alert } from "@material-ui/lab";
 import { useAppSelector } from "redux/app/hooks";
 import { isConnected } from "redux/slices/op25/op25Slice";
 
-import { makeStyles, createStyles, Theme } from "@material-ui/core";
+import { makeStyles, createStyles, Theme, Snackbar } from "@material-ui/core";
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((_theme: Theme) =>
   createStyles({
-    root: {
-      paddingBottom: 20,
+    spaced: {
+      marginBottom: 20,
     },
   })
 );
 
 const GlobalAlerts = () => {
+  const [snackbarReconnectedOpen, setSnackbarReconnectedOpen] = useState(false);
+  const [holdAlerts, setHoldAlerts] = useState(true);
   const isAppConnected = useAppSelector(isConnected);
   const classes = useStyles();
 
+  const openSnackbarReconnected = () => {
+    setSnackbarReconnectedOpen(true);
+  };
+
+  const closeSnackbarReconnected = () => {
+    setSnackbarReconnectedOpen(false);
+  };
+
+  useEffect(() => {
+    if (isAppConnected && !holdAlerts) {
+      openSnackbarReconnected();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAppConnected]);
+
+  useEffect(() => {
+    // This prevents a reconnected message when the interface is first loaded.
+    setTimeout(() => {
+      setHoldAlerts(false);
+    }, 5000);
+  }, []);
+
+  const handleSnackbarReconnectedClose = (
+    _event?: SyntheticEvent,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    closeSnackbarReconnected();
+  };
+
   return (
-    <div className={classes.root}>
-      {isAppConnected !== undefined && !isAppConnected && (
-        <>
-          <Alert variant="filled" severity="error">
-            The OP25 web gui does not have a connection with the python HTTP
-            server.
-          </Alert>
-        </>
+    <>
+      {!holdAlerts && isAppConnected !== undefined && !isAppConnected && (
+        <Alert className={classes.spaced} variant="filled" severity="error">
+          The OP25 web interface does not have a connection with the Python HTTP
+          server.
+        </Alert>
       )}
-    </div>
+      {snackbarReconnectedOpen && (
+        <Snackbar
+          open={snackbarReconnectedOpen}
+          autoHideDuration={6000}
+          onClose={handleSnackbarReconnectedClose}
+        >
+          <Alert
+            variant="filled"
+            severity="success"
+            onClose={handleSnackbarReconnectedClose}
+          >
+            OP25 web interface reconnected with Python HTTP server.
+          </Alert>
+        </Snackbar>
+      )}
+    </>
   );
 };
 
