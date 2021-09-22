7d24ae686632a36a233222ed4036a96eb4de92b4
Gabriel Graves <nebraskacoder@gmail.com>
2021-09-22 00:57:08 -0500

    Interface Tests

--- op25/gr-op25_repeater/www/op25-new-ui/src/redux/slices/interface/interfaceSlice.spec.ts
+++ op25/gr-op25_repeater/www/op25-new-ui/src/redux/slices/interface/interfaceSlice.spec.ts
@@ -1,24 +1,84 @@
-import { InterfaceState } from "../../../types/InterfaceState";
-import interfaceReducer, { gotoTalkgroup } from "./interfaceSlice";
+import interfaceReducer, {
+  initialState,
+  setMenuDrawerOpen,
+  toggleMenuDrawerOpen,
+  toggleMobileMenuDrawerOpen,
+  setMobileMenuDrawerOpen,
+  togglePreferencesDrawerOpen,
+  setPreferencesDrawerOpen,
+} from "./interfaceSlice";
 
-describe("counter reducer", () => {
-  const initialState: InterfaceState = {
-    activeFrequency: undefined,
-    activeTalkgroup: undefined,
-    activeGroupAddress: undefined,
-    activeSourceAddress: undefined,
-  };
-  it("should handle initial state", () => {
-    expect(interfaceReducer(undefined, { type: "unknown" })).toEqual({
-      activeFrequency: undefined,
-      activeTalkgroup: undefined,
-      activeGroupAddress: undefined,
-      activeSourceAddress: undefined,
+describe("interface reducer", () => {
+  describe("menu drawer", () => {
+    it("should handle toggling open and close", () => {
+      const doAction = interfaceReducer(initialState, toggleMenuDrawerOpen());
+      expect(doAction.isMenuDrawerOpen).toEqual(!initialState.isMenuDrawerOpen);
+    });
+
+    it("should handle opening", () => {
+      const doAction = interfaceReducer(initialState, setMenuDrawerOpen(true));
+      expect(doAction.isMenuDrawerOpen).toEqual(true);
+    });
+
+    it("should handle closing", () => {
+      const doAction = interfaceReducer(initialState, setMenuDrawerOpen(false));
+      expect(doAction.isMenuDrawerOpen).toEqual(false);
     });
   });
 
-  it("should handle going to a talkgroup", () => {
-    const actual = interfaceReducer(initialState, gotoTalkgroup(123));
-    expect(actual.activeTalkgroup).toEqual("123");
+  describe("mobile menu drawer", () => {
+    it("should handle toggling open and close", () => {
+      const doAction = interfaceReducer(
+        initialState,
+        toggleMobileMenuDrawerOpen()
+      );
+      expect(doAction.isMobileMenuDrawerOpen).toEqual(
+        !initialState.isMobileMenuDrawerOpen
+      );
+    });
+
+    it("should handle opening", () => {
+      const doAction = interfaceReducer(
+        initialState,
+        setMobileMenuDrawerOpen(true)
+      );
+      expect(doAction.isMobileMenuDrawerOpen).toEqual(true);
+    });
+
+    it("should handle closing", () => {
+      const doAction = interfaceReducer(
+        initialState,
+        setMobileMenuDrawerOpen(false)
+      );
+      expect(doAction.isMobileMenuDrawerOpen).toEqual(false);
+    });
+  });
+
+  describe("preferences drawer", () => {
+    it("should handle toggling open and close", () => {
+      const doAction = interfaceReducer(
+        initialState,
+        togglePreferencesDrawerOpen()
+      );
+      expect(doAction.isPreferencesDrawerOpen).toEqual(
+        !initialState.isPreferencesDrawerOpen
+      );
+    });
+
+    it("should handle opening", () => {
+      const doAction = interfaceReducer(
+        initialState,
+        setPreferencesDrawerOpen(true)
+      );
+      expect(doAction.isPreferencesDrawerOpen).toEqual(true);
+    });
+
+    it("should handle closing", () => {
+      const doAction = interfaceReducer(
+        initialState,
+        setPreferencesDrawerOpen(false)
+      );
+      expect(doAction.isPreferencesDrawerOpen).toEqual(false);
+    });
   });
 });
