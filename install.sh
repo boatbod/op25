33c4485acf6728e626fba19387a1c1c70c329c42
gnorbury <gnorbury@bondcar.com>
2021-11-13 19:59:21 -0500

    Add check to fix bad libairspy0 udev rule TAG+="uaccess"

--- install.sh
+++ install.sh
@@ -33,10 +33,18 @@ else
 
 fi
 
+# blacklist rtl dtv drivers
 if [ ! -f /etc/modprobe.d/blacklist-rtl.conf ]; then
 	echo ====== installing blacklist-rtl.conf
 	echo ====== please reboot before running op25
 	sudo install -m 0644 ./blacklist-rtl.conf /etc/modprobe.d/
+fi
+
+# fix borked airspy udev rule to allow used of airspy device when running headless
+if [ -f /lib/udev/rules.d/60-libairspy0.rules ]; then
+    echo ====== fixing libairspy0 udev rule
+	echo ====== please reboot before running op25
+    sudo sed -i 's^TAG+="uaccess"^MODE="660", GROUP="plugdev"^g' /lib/udev/rules.d/60-libairspy0.rules
 fi
 
 rm -rf build
