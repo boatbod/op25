a1ffc891e0828983bfbc1fb45b3997906b881cb7
gnorbury <gnorbury@bondcar.com>
2022-01-29 15:07:44 -0500

    Changed sdrhwr_removed.log location to /var/log

--- op25/gr-op25_repeater/apps/sdrhw_removed.sh
+++ op25/gr-op25_repeater/apps/sdrhw_removed.sh
@@ -3,6 +3,6 @@
 # This script gratuitously kills all rx.py and multi_rx.py processes
 # It is intended to be invoked by a UDEV rule when a SDR device disconnects
 #
-LOG=$(pwd)/$(basename $0 | sed 's/[.]sh$/.log/')
+LOG=/var/log/$(basename $0 | sed 's/[.]sh$/.log/')
 echo "$(date): $(basename $0): killing op25, cmd args: $@" >> ${LOG}
 kill $(pgrep -f rx.py)
