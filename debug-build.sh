f8b23a3cc18a19b7e4f093c7f722724e0fbe70e1
gnorbury <gnorbury@bondcar.com>
2023-01-08 15:04:12 -0500

    Updated for gr-3.10

--- debug-build.sh
+++ debug-build.sh
@@ -2,7 +2,10 @@
 git pull
 cd build
 rm -rf *
+#cmake ../ -DCMAKE_BUILD_TYPE=Debug \
+#          -DCMAKE_CXX_FLAGS="-fsanitize=address -fno-omit-frame-pointer" \
+#          -DCMAKE_LINK_OPTIONS=-fsanitize=address 2>&1 | tee cmake.log
 cmake ../ -DCMAKE_BUILD_TYPE=Debug 2>&1 | tee cmake.log
-make                               2>&1 | tee make.log
+make -j4 VERBOSE=1                 2>&1 | tee make.log
 sudo make install                  2>&1 | tee install.log
 sudo ldconfig
