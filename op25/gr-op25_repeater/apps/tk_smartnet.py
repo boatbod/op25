7a5b081c5df990a5173379c642418d8f677f8bfa
ilyacodes <ilya@ilya.is>
2024-04-18 07:43:51 -0400

    SmartNet: denied omnilink trespass message

--- op25/gr-op25_repeater/apps/tk_smartnet.py
+++ op25/gr-op25_repeater/apps/tk_smartnet.py
@@ -1126,6 +1126,11 @@ class osw_receiver(object):
                             src_rid = osw2_addr
                             if self.debug >= 11:
                                 sys.stderr.write("%s [%d] SMARTNET DENIED CALL ALERT TARGET OFFLINE src(%05d)\n" % (log_ts.get(), self.msgq_id, src_rid))
+                        # OmniLink trespass rejected
+                        elif osw1_addr == 0x2c60:
+                            src_rid = osw2_addr
+                            if self.debug >= 11:
+                                sys.stderr.write("%s [%d] SMARTNET DENIED OMNILINK TRESPASS src(%05d)\n" % (log_ts.get(), self.msgq_id, src_rid))
                         # Failsoft assign
                         elif osw1_addr == 0x8301:
                             tgt_rid = osw2_addr
