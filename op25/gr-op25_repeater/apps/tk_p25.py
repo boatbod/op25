d893ef497ea97a2d15d8e837e268efc0e485143c
triptolemus510 <152668894+triptolemus510@users.noreply.github.com>
2025-04-16 17:00:13 -0700

    Add files via upload

--- op25/gr-op25_repeater/apps/tk_p25.py
+++ op25/gr-op25_repeater/apps/tk_p25.py
@@ -1669,8 +1669,27 @@ class p25_system(object):
             else:
                 d['frequencies'][f] = '- %f  %s [               ]  %s  count %d' % ((f / 1e6), f_type, time_ago_ncurses_str, count)
 
+            tags = []
+            sources = []
+
+            for tgid in tgids:
+                 try:
+                     tgid_int = int(tgid)
+                     tag = self.talkgroups.get(tgid_int, {}).get('tag', None)
+                     
+                     srcaddr = self.talkgroups.get(tgid_int, {}).get('srcaddr', 100)
+                     if srcaddr == 0:
+                         srcaddr = 99999999
+                     sources.append(srcaddr)
+                     sys.stderr.write(f"shit: {srcaddr}\n")
+                 except (ValueError, TypeError) as e:
+                     if self.debug >= 10:
+                         sys.stderr.write(f"Error converting TGID '{tgid}' to int: {e}\n")
+                     tag = None
+                 tags.append(tag)
+
             # The easy part: send pure JSON and let the display layer handle formatting
-            d['frequency_data'][f] = {'type': chan_type, 'tgids': tgids, 'last_activity': time_ago_str, 'counter': count}
+            d['frequency_data'][f] = {'type': chan_type, 'tgids': tgids, 'last_activity': time_ago_str, 'counter': count, 'tags': tags, 'srcaddr': sources}
 
         # Patches
         self.expire_patches()
