ae3f92e022becf03eaea0b7668645a07c8f924c8
Graham Norbury <gnorbury@bondcar.com>
2026-03-21 17:21:36 -0400

    PDU logging tweak

--- op25/gr-op25_repeater/lib/p25p2_tdma.cc
+++ op25/gr-op25_repeater/lib/p25p2_tdma.cc
@@ -174,11 +174,9 @@ int p25p2_tdma::process_mac_pdu(const uint8_t byte_buf[], const unsigned int len
     unsigned int opcode = (byte_buf[0] >> 5) & 0x7;
 
     if (d_debug >= 10) {
-        char strbuf[255];
-        unsigned int spos = 0;
-        for (unsigned int i=0; i<len; i++) {
-            sprintf(strbuf+spos, "%02x ", byte_buf[i]);
-            spos += 3;
+        char strbuf[69];
+        for (unsigned int i = 0; (i < len) && (i < 23); i++) {
+            sprintf(strbuf+(3 * i), "%02x ", byte_buf[i]);
         }
         fprintf(stderr, "%s process_mac_pdu: opcode %d offset %d len %d pdu %s\n", logts.get(d_msgq_id), opcode, offset, len, strbuf);
     }
